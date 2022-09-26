8053664a921bda0c8ba02f960b92c379a396fa1e
fraanntadeo <thefranco65@hotmail.com>
2022-09-26 18:42:53 -0300

    Push

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,44 @@
+import "./contact.css";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+
+function Contact() {
+  return (
+    <div id="contact" className="container contact-container">
+      <h1>Contact Me</h1>
+      <div className="contact-links">
+        <a
+          href="https://www.youtube.com/"
+          className="contact youtube"
+          target={"blank"}
+        >
+          <AiOutlineYoutube className="icon" />
+          <h2>
+            youtube <span>PROXIMAMENTE</span>
+          </h2>
+        </a>
+
+        <a
+          href="https://www.whatsapp.com/"
+          className="contact whatsapp"
+          target={"blank"}
+        >
+          <AiOutlineWhatsApp className="icon" />
+          <h2>
+            whatsapp <span>PROXIMAMENTE</span>
+          </h2>
+        </a>
+
+        <a href="/" className="contact instagram">
+          <AiOutlineInstagram className="icon" />
+          <h2>
+            instagram <span>PROXIMAMENTE</span>
+          </h2>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+function Header() {
+  return (
+    <div id='home' className='container header-container'>
+      <h3>
+        Thynuviel
+      </h3>
+      <h2>Front-End Web Developer</h2>
+    </div>
+  );
+}
+export default Header;
